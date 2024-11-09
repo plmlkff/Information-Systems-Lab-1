ad407f9fbd671896b4b6edb1f439191857392884
pavelmalkov <pashamalkovich@mail.ru>
2024-11-09 16:21:12 +0300

    fix: Now CORSFilter returns 200 for OPTIONS request

--- src/main/java/ru/itmo/is_lab1/security/filter/CORSFilter.java
+++ src/main/java/ru/itmo/is_lab1/security/filter/CORSFilter.java
@@ -3,6 +3,7 @@ package ru.itmo.is_lab1.security.filter;
 import jakarta.annotation.Priority;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
@@ -12,10 +13,15 @@ import java.io.IOException;
 public class CORSFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "*");
-        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        if (request.getMethod().equals("OPTIONS")){
+            response.setStatus(200);
+            return;
+        }
         chain.doFilter(servletRequest, servletResponse);
     }
 }
